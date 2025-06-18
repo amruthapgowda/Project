4272a830e4730b6a1fc5a3b72e02112fb68eab62
Amrutha <elixirapg@gmail.com>
2025-06-18 12:12:15 +0530

    Updated app.js and map.js

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/streets-v12",
-    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position lng, lat. lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
 
